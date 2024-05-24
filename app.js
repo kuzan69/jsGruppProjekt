73352b52ec88cbe3439b323977f6b8f20f5150cb
kuzan69 <113890965+kuzan69@users.noreply.github.com>
2024-05-24 02:33:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // Game state - Salih del 
+let clickCount = 0;
 let currentScore = 0;
 let activePlayer = 0;
 let totalScores = [0, 0];
 const winningScore = 56;
+
+const hideBackground = document.querySelector('.hideBackground');
+const x = document.querySelector('#x');
+const gameSteps = document.querySelector('#gameSteps');
+const prevNext = document.querySelector('.prevNext');
+const previous = document.querySelector('#previous');
+const nextDummy = document.querySelector('#nextDummy');
+const next = document.querySelector('#next');
 
 // Referens till alla element i DOM - Salih
 const btnRoll = document.querySelector('.btnRoll');
@@ -18,6 +27,65 @@ const playerTwo = document.querySelector('#playerTwo');
 
 const playerText = document.querySelector('#playerText');
 const pointText = document.querySelector('#pointText');
+
+const imgOne = document.createElement('img');
+imgOne.src = 'assets/4.png';
+imgOne.classList.add('diceImage');
+imgOne.style.display = 'block';
+imgOne.style.margin = '10px auto';
+
+const imgTwo = document.createElement('img');
+imgTwo.src = 'assets/1.png';
+imgTwo.classList.add('diceImage');
+imgTwo.style.display = 'block';
+imgTwo.style.margin = '10px auto';
+
+const steps = [
+  'The game starts when Player 1 starts rolling the dice',
+  'Let us say that you rolled a 4',
+  'Good. Now, do you want to keep on rolling.. Or do you want to keep your points?',
+  'Damn, I rolled and got the CURSED Dice, NUMERO UNO!!',
+  'Now.. Want to know the limit? It is now your turn to play!',
+  'Good luck!'
+];
+
+x.addEventListener('click', () => {
+  hideBackground.style.display = 'none';
+  clickCount = 0;
+});
+
+nextDummy.addEventListener('click', () => {
+  gameSteps.textContent = steps[clickCount++];
+  prevNext.style.display = 'flex';
+});
+
+next.addEventListener('click', () => {
+  gameSteps.textContent = steps[clickCount++];
+  if(clickCount === steps.length + 1) {
+    hideBackground.style.display = 'none';
+  }
+  if(clickCount === 2) {
+    gameSteps.append(imgOne);
+  }
+  if(clickCount === 4) {
+    gameSteps.append(imgTwo);
+    gameSteps.append('Now it is Player 2s turn.')
+  }
+  if(clickCount === steps.length) {
+    gameStyle = gameSteps.style;
+    gameStyle.fontSize = '2.25rem';
+    gameStyle.fontWeight = 'bold';
+  }
+  if(clickCount <= 1) {
+    previous.addEventListener('click', () => {
+      gameSteps.textContent = steps[clickCount--];
+    });
+  }
+});
+
+previous.addEventListener('click', () => {
+  steps[clickCount]
+});
 
 // Objekt med bilder, varje bild ska ha en key med nummer, valuen ska vara src - Salihs del igen
 const diceImages = {
@@ -103,22 +171,26 @@ btnHold.addEventListener('click', () => {
 // När man startar ett nytt spel Salihs del
 newGameAll.forEach(game => {
   game.addEventListener('click', () => {
-    diceImage.style.display = 'none';
-    totalScores = [0, 0];
-    activePlayer = 0;
-    currentScore = 0;
+    // hideBackground.style.display = 'grid';
+    // clickCount = 0;
+    // diceImage.style.display = 'none';
+    // totalScores = [0, 0];
+    // activePlayer = 0;
+    // currentScore = 0;
   
-    firstPoint.textContent = '0';
-    secondPoint.textContent = '0';
-    currentText.textContent = '';
-    playerText.textContent = '';
-    pointText.textContent = '';
-    diceImage.src = '';
+    // firstPoint.textContent = '0';
+    // secondPoint.textContent = '0';
+    // currentText.textContent = '';
+    // playerText.textContent = '';
+    // pointText.textContent = '';
+    // diceImage.src = '';
   
-    btnRoll.disabled = false;
-    btnHold.disabled = false;
-  
-    updatePlayerHighlight(); 
+    // btnRoll.disabled = false;
+    // btnHold.disabled = false;
+
+    // updatePlayerHighlight(); 
+
+    location.reload();
   });
 });
 
@@ -144,4 +216,3 @@ newGameAll.forEach(game => {
 // });
 
 updatePlayerHighlight(); 
-
